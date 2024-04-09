48e19a7d8ec61e8e75217a0674a1d8e89fbe8fd2
Aricoins <arielgarcia79@gmail.com>
2024-04-08 23:48:04 -0300

    path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import path from 'path'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '',
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        nested: path.resolve(__dirname,  'index.html'),
+       
+      },
+    },
+}})

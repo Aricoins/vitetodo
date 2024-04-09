33c8827e4025885494a733a1469a197fbaeb59aa
Aricoins <arielgarcia79@gmail.com>
2024-04-08 22:46:27 -0300

    2246

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './views/LoginPage'
+
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+       <Routes>
+          <Route path="/" element={<h1>  Hola </h1>} />
+          <Route path="/login" element={<LoginPage />} />
+      
+          </Routes>
+          </BrowserRouter>
+    
+    </>
+  )
+}
+
+export default App
